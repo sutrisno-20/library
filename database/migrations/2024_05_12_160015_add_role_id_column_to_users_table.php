af73f46a35a95fcca20887c42148894502b61ffa
Sutrisno <masutrisn021@gmail.com>
2024-05-17 09:47:37 +0700

    feat:making tables migration

--- database/migrations/2024_05_12_160015_add_role_id_column_to_users_table.php
+++ database/migrations/2024_05_12_160015_add_role_id_column_to_users_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
+            // add field after field password
             $table->after('password', function ($table) {
                 $table->unsignedBigInteger('role_id');
                 $table->foreign('role_id')->references('id')->on('roles');
