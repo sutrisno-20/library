3e39870026aad6f2861574eaaade6eb9b3269fae
Sutrisno <masutrisn021@gmail.com>
2024-05-22 14:45:08 +0700

    feat: creating page login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
     public function login(Request $r) {
+        return view('login');
+    }
 
+    public function register() {
+        return view('register');
     }
 }
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,8 +5,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Library || Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <style>
+        .login-box {
+            width: 500px;
+            border: 1px solid;
+            padding: 30px;
+        }
+        form div {
+            margin-bottom: 15px;
+        }
+    </style>
   </head>
   <body>
+
+    <div class="main vh-100 d-flex justify-content-center align-items-center">
+        <div class="login-box">
+            <form action="" method="post">
+                @csrf
+                <div>
+                    <label for="username" class="form-label">Username</label>
+                    <input type="text" name="username" id="username" class="form-control">
+                </div>
+                <div>
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" name="password" id="password" class="form-control">
+                </div>
+                <div>
+                    <button type="submit" class="btn btn-primary form-control">Login</button>
+                </div>
+                <div class="text-center">
+                    <a href="register">Sign Up</a>
+                </div>
+            </form>
+        </div>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   </body>
